16300b6a36552c80103dd8c38d02d9a7f8e4bbac
soruly <soruly@gmail.com>
2018-02-09 14:26:25 +0800

    Use hostname as index

--- update.php
+++ update.php
@@ -7,16 +7,20 @@ require 'vendor/autoload.php';
 
 $client = ClientBuilder::create()->setHosts(['192.168.2.12'])->build();
 
-//$response = $client->indices()->delete(['index' => 'files']);
+$index = explode(".", gethostname())[0];
+
+if ($client->indices()->exists(['index' => $index])) {
+  $client->indices()->delete(['index' => $index]);
+}
 
 $params = [
-	'index' => 'files',
-	'body' => [
-		'settings' => [
-			'number_of_shards' => 3,
-			'number_of_replicas' => 0
-		]
-	]
+  'index' => $index,
+  'body' => [
+    'settings' => [
+      'number_of_shards' => 3,
+      'number_of_replicas' => 0
+    ]
+  ]
 ];
 
 $response = $client->indices()->create($params);
@@ -27,30 +31,30 @@ $params = [];
 $linecount = 0;
 $handle = fopen($file, "r");
 while(!feof($handle)){
-	$linecount++;
-	$line = fgets($handle);
+  $linecount++;
+  $line = fgets($handle);
 
-	$params['index'] = 'files';
-	$params['type']  = 'file';
-	$params['body'][] = [
-		'index' => [
-			'_id' => $linecount
-		]
-	];
+  $params['index'] = $index;
+  $params['type']  = 'file';
+  $params['body'][] = [
+    'index' => [
+      '_id' => $linecount
+    ]
+  ];
 
-	$params['body'][] = [
-		'filename' => $line
-	];
-	if($linecount % 10000 == 0){
-		echo $linecount,"\n";
-		$ret = $client->bulk($params);
-		$params = [];
-	}
+  $params['body'][] = [
+    'filename' => $line
+  ];
+  if($linecount % 10000 == 0){
+    echo $linecount,"\n";
+    $ret = $client->bulk($params);
+    $params = [];
+  }
 }
 if($linecount % 10000 != 0){
-	echo $linecount,"\n";
-	$ret = $client->bulk($params);
-	$params = [];
+  echo $linecount,"\n";
+  $ret = $client->bulk($params);
+  $params = [];
 }
 fclose($handle);
 echo 'Index update took ', time() - $time, ' seconds', "\n";
