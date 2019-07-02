c52721bf9d9976f0c1b9816ae13326c8ff54a379
soruly <soruly@gmail.com>
2019-07-02 13:49:52 +0800

    Correct code format

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/update", async (req, res) => {
   res.write("Remove existing index...\n");
   const resultDelete = await fetch("http://elasticsearch:9200/files", {
     method: "DELETE"
-  }).then((response) => response.json());
+  }).then(response => response.json());
   res.write(`${JSON.stringify(resultDelete)}\n`);
 
   res.write("Creating new index...\n");
@@ -29,15 +29,15 @@ app.get("/update", async (req, res) => {
         }
       }
     }),
-    headers: {"Content-Type": "application/json"}
-  }).then((response) => response.json());
+    headers: { "Content-Type": "application/json" }
+  }).then(response => response.json());
   res.write(`${JSON.stringify(result)}\n`);
   res.write("Reading filelist...\n");
   const data = await fs.readFile("list.txt", "utf-8");
   const fileList = data.split("\n");
 
   res.write("Indexing...\n");
-  const asyncTask = async function *() {
+  const asyncTask = async function*() {
     const bulkSize = 10000;
     let start = 0;
     let end = 0;
@@ -46,15 +46,15 @@ app.get("/update", async (req, res) => {
       end = end > fileList.length ? fileList.length : end;
       const command = [];
       for (let i = start; i < end; i += 1) {
-        command.push({index: {_id: start + i + 1}});
-        command.push({filename: fileList[start + i]});
+        command.push({ index: { _id: start + i + 1 } });
+        command.push({ filename: fileList[start + i] });
       }
-      const body = `${command.map((obj) => JSON.stringify(obj)).join("\n")}\n`;
+      const body = `${command.map(obj => JSON.stringify(obj)).join("\n")}\n`;
       await fetch("http://elasticsearch:9200/files/file/_bulk", {
         method: "POST",
         body,
-        headers: {"Content-Type": "application/x-ndjson"}
-      }).then((respond) => respond.json());
+        headers: { "Content-Type": "application/x-ndjson" }
+      }).then(respond => respond.json());
       yield end;
       start = end;
     }
@@ -62,36 +62,41 @@ app.get("/update", async (req, res) => {
   for await (const end of asyncTask()) {
     res.write(`${end}\n`);
   }
-  res.write(`Index update took ${((Date.now() - startTime) / 1000).toFixed(2)} seconds for ${fileList.length} records\n`);
+  res.write(
+    `Index update took ${((Date.now() - startTime) / 1000).toFixed(
+      2
+    )} seconds for ${fileList.length} records\n`
+  );
   res.end();
 });
 
 app.get("/", async (req, res) => {
   const from = parseInt(req.query.from, 10) || 0;
   const size = 50;
-  const json = req.query.q ? {
-    from,
-    size,
-    query: {
-      bool: {
-        must: {
-          match: {
-            filename: {
-              query: req.query.q,
-              operator: "or"
+  const json = req.query.q
+    ? {
+        from,
+        size,
+        query: {
+          bool: {
+            must: {
+              match: {
+                filename: {
+                  query: req.query.q,
+                  operator: "or"
+                }
+              }
             }
           }
         }
       }
-    }
-  } : {from,
-    size};
+    : { from, size };
 
   const results = await fetch("http://elasticsearch:9200/files/file/_search", {
     method: "POST",
     body: JSON.stringify(json),
-    headers: {"Content-Type": "application/json"}
-  }).then((response) => response.json());
+    headers: { "Content-Type": "application/json" }
+  }).then(response => response.json());
 
   res.render("index", {
     q: req.query.q,
@@ -102,4 +107,3 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(port);
-
